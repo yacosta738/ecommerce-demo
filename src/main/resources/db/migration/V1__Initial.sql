13bed4d0005ba3246a1c725fef30977d2c1a266d
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2022-12-03 19:47:09 -0300

    develop < feature(core): Add H2 with migrations support 💽 (#3)
    * feature(core): Add H2 with migrations support 💽
    
    Configure H2 and Flyway with a basic schema

---
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,44 @@
+-- En la base de datos de comercio electrónico de la compañía disponemos de la tabla PRICES que refleja el precio final (pvp) y la tarifa que aplica a un producto de una cadena entre unas fechas determinadas. A continuación se muestra un ejemplo de la tabla con los campos relevantes:
+--
+-- PRICES
+-- -------
+--
+-- BRAND_ID         START_DATE                                    END_DATE                        PRICE_LIST                   PRODUCT_ID  PRIORITY                 PRICE           CURR
+-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- 1         2020-06-14-00.00.00                        2020-12-31-23.59.59                        1                        35455                0                        35.50            EUR
+-- 1         2020-06-14-15.00.00                        2020-06-14-18.30.00                        2                        35455                1                        25.45            EUR
+-- 1         2020-06-15-00.00.00                        2020-06-15-11.00.00                        3                        35455                1                        30.50            EUR
+-- 1         2020-06-15-16.00.00                        2020-12-31-23.59.59                        4                        35455                1                        38.95            EUR
+--
+-- Campos:
+--
+-- BRAND_ID: foreign key de la cadena del grupo (1 = ZARA).
+-- START_DATE , END_DATE: rango de fechas en el que aplica el precio tarifa indicado.
+-- PRICE_LIST: Identificador de la tarifa de precios aplicable.
+-- PRODUCT_ID: Identificador código de producto.
+-- PRIORITY: Desambiguador de aplicación de precios. Si dos tarifas coinciden en un rago de fechas se aplica la de mayor prioridad (mayor valor numérico).
+-- PRICE: precio final de venta.
+-- CURR: iso de la moneda.
+
+-- Creación de la tabla PRICES
+CREATE TABLE PRICES
+(
+    BRAND_ID    INT,
+    START_DATE  TIMESTAMP,
+    END_DATE    TIMESTAMP,
+    PRICE_LIST  INT,
+    PRODUCT_ID  INT,
+    PRIORITY    INT,
+    PRICE       DECIMAL(10, 2),
+    CURR        VARCHAR(3)
+);
+
+-- Inserción de datos de ejemplo
+INSERT INTO PRICES (BRAND_ID, START_DATE, END_DATE, PRICE_LIST, PRODUCT_ID, PRIORITY, PRICE, CURR)
+VALUES (1, '2020-06-14 00.00.00', '2020-12-31 23.59.59', 1, 35455, 0, 35.50, 'EUR');
+INSERT INTO PRICES (BRAND_ID, START_DATE, END_DATE, PRICE_LIST, PRODUCT_ID, PRIORITY, PRICE, CURR)
+VALUES (1, '2020-06-14 15.00.00', '2020-06-14 18.30.00', 2, 35455, 1, 25.45, 'EUR');
+INSERT INTO PRICES (BRAND_ID, START_DATE, END_DATE, PRICE_LIST, PRODUCT_ID, PRIORITY, PRICE, CURR)
+VALUES (1, '2020-06-15 00.00.00', '2020-06-15 11.00.00', 3, 35455, 1, 30.50, 'EUR');
+INSERT INTO PRICES (BRAND_ID, START_DATE, END_DATE, PRICE_LIST, PRODUCT_ID, PRIORITY, PRICE, CURR)
+VALUES (1, '2020-06-15 16.00.00', '2020-12-31 23.59.59', 4, 35455, 1, 38.95, 'EUR');
